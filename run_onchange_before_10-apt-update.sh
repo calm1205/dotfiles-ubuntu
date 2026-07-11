69eb9374c44bdaa7dae24b669c2eae49d661e8d0
calm1205 <authentic.nagi@gmail.com>
2026-07-11 22:17:05 +0900

    feat: apt更新・パッケージインストールスクリプトを追加

---
+++ run_onchange_before_10-apt-update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -eufo pipefail
+
+sudo apt update
---
+++ run_onchange_before_20-apt-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -eufo pipefail
+
+sudo apt install -y fzf zoxide zsh keyd neovim ripgrep wl-clipboard zsh-autosuggestions gh
