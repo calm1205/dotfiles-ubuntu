d795feadbc741640ad9d9d6a892b336c55f01034
calm1205 <authentic.nagi@gmail.com>
2026-07-12 15:29:48 +0900

    feat: GNOME入力設定の適用スクリプトを追加

---
+++ run_onchange_after_50-gnome-input-settings.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -eufo pipefail
+
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 17
+gsettings set org.gnome.desktop.peripherals.keyboard delay 312
+gsettings set org.gnome.desktop.peripherals.mouse speed 1.0
